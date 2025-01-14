18cdcab18a24c3c0b6100609f862c04b230ef15f
Salvador <salgarmol@alu.edu.gva.es>
2025-01-14 18:46:19 +0100

    subiendo archivo java por Salvador

---
+++ MiClase1.java
@@ -0,0 +1,10 @@
+public class MiClase1
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
